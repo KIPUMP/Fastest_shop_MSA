ca818a45632d11438c6976c768a7f2f6ce74ef8e
KIPUMP <kibeom2000@naver.com>
2024-09-06 18:54:38 +0900

    refactor (docker-compose, RedissionConfig) file

--- product-service/src/main/java/com/shop/product_service/config/RedissionConfig.java
+++ product-service/src/main/java/com/shop/product_service/config/RedissionConfig.java
@@ -11,7 +11,8 @@ public class RedissionConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
+//      config.useSingleServer().setAddress("redis://localhost:6379"); // local
+        config.useSingleServer().setAddress("redis://redis:6379");  // docker
         return Redisson.create(config);
     }
 }
